7f1a1929b87cc0b1b78c9277666054d234b43cc2
Milena <morozowamilwna@yandex.ru>
2021-01-30 11:58:06 +0300

    update CustomUser model

--- authentication/models.py
+++ authentication/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, User
+from django.utils import timezone
 
 from .account_colors import random_color
 
@@ -10,6 +11,7 @@ class CustomAccountManager(BaseUserManager):
                           email=email,
                           password=password,
                           account_color=random_color())
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
@@ -24,9 +26,10 @@ class CustomAccountManager(BaseUserManager):
 class CustomUser(AbstractBaseUser):
     username = models.CharField(unique=True, max_length=200)
     email = models.EmailField(unique=True)
-    password = models.CharField(unique=True, max_length=128)
+    # password = models.CharField(unique=True, max_length=128)
     account_color = models.CharField(max_length=7)
     is_confirmed = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     objects = CustomAccountManager()
 
