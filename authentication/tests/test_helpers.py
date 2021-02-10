941451f8ed58a00225f4eff20e92ddf78e2eb2fa
Milena <morozowamilwna@yandex.ru>
2021-02-10 21:59:52 +0300

    update test_helper

--- authentication/tests/test_helpers.py
+++ authentication/tests/test_helpers.py
@@ -1,17 +1,20 @@
+from django.http import HttpResponse
 from django.test import TestCase, RequestFactory
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 from ..helper import catch_view_exception
 from helpers.logger import create_logger
 
-
 test_logger = create_logger('test_helper')
 
 
+@api_view(['POST'])
 @catch_view_exception(('username', 'email'), test_logger)
-def test_view(username, email, **kwargs):
-    if not username:
+def test_view(request):
+    if not request.data['username']:
         raise Exception()
-    return 1
+    return HttpResponse(status=status.HTTP_200_OK)
 
 
 class HelperTests(TestCase):
@@ -21,31 +24,31 @@ class HelperTests(TestCase):
     def test_bad_type_request_put(self):
         request = self.factory.put('/tests/helper')
         response = test_view(request)
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.content, b'Bad request type')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(str(response.data['detail']), 'Method "PUT" not allowed.')
 
     def test_no_username_in_params(self):
-        request = self.factory.get('/tests/helper', {'email': 'masht@mail.ru'})
+        request = self.factory.post('/tests/helper', {'email': 'masht@mail.ru'})
         response = test_view(request)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.content, b'username not given')
 
     def test_unnecessary_param(self):
-        request = self.factory.get('/tests/helper', {'username': 'Igor',
+        request = self.factory.post('/tests/helper', {'username': 'Igor',
                                                       'email': 'masht@mail.ru',
                                                       'password': '12345'})
         response = test_view(request)
-        self.assertEqual(response, 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_another_exception(self):
-        request = self.factory.get('/tests/helper', {'username': '',
+        request = self.factory.post('/tests/helper', {'username': '',
                                                      'email': 'masht@mail.ru'})
         response = test_view(request)
-        self.assertEqual(response.status_code, 500)
+        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
         self.assertTrue(b'problem in server' in response.content)
 
     def test_ok(self):
-        request = self.factory.get('/tests/helper', {'username': 'Igor',
+        request = self.factory.post('/tests/helper', {'username': 'Igor',
                                                      'email': 'masht@mail.ru'})
         response = test_view(request)
-        self.assertEqual(response, 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
